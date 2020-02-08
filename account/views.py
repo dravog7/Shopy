905e32b161e5560cddbc787652f2378130464888
Ann <annphilomina246@gmail.com>
2020-02-09 00:35:48 +0530

    add feature

--- account/views.py
+++ account/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.http import HttpResponse
+from django.contrib.auth import logout
 def signin(request):
     username = request.POST['username']
     password = request.POST['password']
@@ -13,3 +14,16 @@ def signin(request):
         else:
             return HttpResponse('0')
 
+    def signup(request):
+        username = request.POST['username']
+        password = request.POST['password']
+        email = request.POST['email']
+        user = User.objects.create_user(username,email,password)
+        user=authenticate(username=username,password=password)
+        if user is None:
+            login(request,user)
+        else:
+            return HttpResponse('0')
+    
+    def signout(request)
+      logout(request)
